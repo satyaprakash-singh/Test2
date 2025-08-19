b29e2dfb902d4bc09223debffa286c5d725db4f7
satyaprakash-singh <satya199440@gmail.com>
2025-08-19 12:47:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "testResourceGroup1"
   location = "Central India"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "testResourceGroup2"
+  location = "Central India"
+}
